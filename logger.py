0060c4b856eeb9c550976b2e5fb8450518de2663
Ervin Picardal <ervinjohnpicardal@gmail.com>
2022-10-27 20:55:38 +0800

    Updated Documentation

--- logger.py
+++ logger.py
@@ -1,19 +1,22 @@
-from datetime import date
+from datetime import datetime
 import json
 
+# Logging the request info
 def reqLog(path, method):
-   today = str(date.today())
+   today = str(datetime.now())
    logItem = f'{today}\t{method}\t{path}'
 
    
    with open ('./logs/logRequest.log', 'a') as writeLog:
       writeLog.write(logItem+"\n")
-   
+
+# Logging the data info into backlogs.json
 def backLogs(data,path,method):
+   
    with open ('./logs/backlog.json', 'r') as jsonLog:
       backlog = json.loads(jsonLog.readline())
    
-      today = str(date.today())
+      today = str(datetime.now())
       
       metadata = {
          "timestamp": today,
@@ -30,4 +33,5 @@ def backLogs(data,path,method):
       
       with open ('./logs/backlog.json', 'w') as writeBackLog:
          writeBackLog.write(json.dumps(backlog))
+
       
